3dcb36421e37c952c0cd11d404e26e280ccfe0bb
mary <Wifi-cable@users.noreply.github.com>
2016-04-18 18:42:14 +0200

    fixing messy variables

--- src/Aufgabe1.java
+++ src/Aufgabe1.java
@@ -7,14 +7,12 @@ public class Aufgabe1 { // should main be inside of class Aufgabe1?
 	public static void main(String[] args) {
 		Scanner bin = new Scanner(System.in);	 // new scanner
 		System.out.println("Bitte eine ganze zahl eingeben um sie in Binär umgewandelt zu bekommen"); //input
-		 public int zahl = bin.nextInt(); //uses the input, assigns varable
-		berechneBin(zahl);
+		int zahlDez = bin.nextInt(); //uses the input, assigns varable
+		berechneBin(zahlDez);
 		}
-		public static void berechneBin(int dez){
-			
-		// how to make it recursive, assingning the zahl variable again and again till a zero is reached?
-		int rest= (dez %2);
-		int ergebniss = dez/2;//divides input by 2 and checks for a rest.
+		public static void berechneBin(int zahl){
+		do while rest <1;
+		int rest= (zahlDez %2);
 		if (dez% 2==1){  // checks for rest
 			System.out.print(1); //shows a 1 if there is a rest
 		}
@@ -23,9 +21,7 @@ public class Aufgabe1 { // should main be inside of class Aufgabe1?
 			System.out.print(0);	//shows  a zero if no rest
 			} // zahl is not a local variable. how to put global ones in local strings? 
 		System.out.print("das ist die Binäre schreibweise"+ ergebniss+ "von" + zahl); 
-/* were to put the next class for the object aufgabe2 and aufgabe3?
-		does that require 3 different classes?*/
-	/* how to track java in git? commit as package?*/
+
 		}
 
 	}
