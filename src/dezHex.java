af3441d9eca005e050345182655e00c0a52d30b1
mary <Wifi-cable@users.noreply.github.com>
2016-04-18 21:23:02 +0200

    last bug/biggest bug. calculation wrong

--- src/dezHex.java
+++ src/dezHex.java
@@ -1,15 +1,18 @@
 import java.util.Scanner;
 public class dezHex {
 	
+	private static Scanner dez;
+
 	public static void main(String[] args) {
+		dez = new Scanner(System.in);
 		System.out.println("bitte ganze dezimalzahl eingeben");
-		Scanner dez = new Scanner(System.in);
 		int dez2Hex= dez.nextInt();
+		
+		int rem;  //remembers or stores remainer
+		String hexMem=""; //stores the result
 		char [] hexDigits ={ '0', '1', '2', '3', '4', '5', '6', '7', 
 				'8', '9', 'A', 'B', 'C', 'D', 'E', 'F'
 				  };
-		int rem;  //remembers or stores remainer
-		String hexMem=""; //stores the result
 		while (dez2Hex >0){		// loop until it is zero
 		rem=dez2Hex%16;	// does modulo 16 on the input
 		hexMem=hexDigits [rem]+hexMem;	//takes the stored remainer the array hexDigits adds remebered result
